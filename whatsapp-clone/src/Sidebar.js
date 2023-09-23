51c0853a785596d13223c229bf378e8d10f56419
Swathi <ssspriyamoru@gmail.com>
2023-09-23 16:58:49 +0530

    new commit

---
+++ whatsapp-clone/src/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://octodex.github.com/images/labtocat.png" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBPYSoOKyLiII623iySQpiO-OXYJckwOVc",
+    authDomain: "whatsapp-mern-cdf33.firebaseapp.com",
+    projectId: "whatsapp-mern-cdf33",
+    storageBucket: "whatsapp-mern-cdf33.appspot.com",
+    messagingSenderId: "1012891732163",
+    appId: "1:1012891732163:web:dbc4559aceb2e3b2953a76",
+    measurementId: "G-Z61NMK1WYG"
+  };
